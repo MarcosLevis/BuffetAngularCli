6f6ac2d86ade240dcaf0d45c587aab47bf15586a
MarcosLevis <marcoslevis.rck@gmail.com>
2024-12-18 20:58:33 -0300

    interceptor localstorage

--- src/app/interceptores/TokenInterceptor.ts
+++ src/app/interceptores/TokenInterceptor.ts
@@ -7,17 +7,14 @@ export class TokenInterceptor implements HttpInterceptor {
 
     constructor(){}
     
-    //Esto existe hasta que se cree el login con el local storage
-    private tokenPrueba: string =  `eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXJjb3NAZ21haWwuY29tIiwiZXhwIjoxNzM0NTYyNTUwfQ.2ULPlYtQTLLcPuU0YgQzpWU2Vj60VfSjXRBdELE3qBc`;
+    
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
 
-        console.log(`TokenInterceptor - ${req.url}`);
-        
+        console.log(`TokenInterceptor - ${req.url}`);        
         let authReq: HttpRequest<any> = req.clone({
             setHeaders:{
-                //Authorization : `Bearer ${localStorage.getItem("token")}`
-                Authorization : `Bearer ${this.tokenPrueba}`
+                Authorization : `Bearer ${localStorage.getItem("accessToken")}`
             }
         });
         return next.handle(authReq); //para no hacer cambios
